1b61f625f411594fffa4b677cc7fc7420885538e
Xhst <82611659+Xhst@users.noreply.github.com>
2025-02-18 10:25:19 +0100

    feat(client): types

---
+++ client/src/types.ts
@@ -0,0 +1,28 @@
+export type Resource = {
+  id: number;
+  size: number;
+};
+
+export type Agent = {
+  id: number;
+  color: string;
+  tasks: Task[];
+};
+
+export type Task = {
+  id: number;
+  size: number;
+  dependencies: number[];
+}
+
+export type SolvingMethod = "greedy" | "ilp" | "local_search | simulated_annealing";
+
+export type Solution = {
+  method: string,
+  solution: [[number, number][]];
+  z: number;
+  time: number;
+  colors: string[];
+  resources: number[];
+  tasks: number[][];
+};
